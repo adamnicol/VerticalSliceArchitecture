9d11992f1026ed14bd1aabcc9ff48bb3bf9f6094
Adam Nicol <adam.nicol@protonmail.com>
2026-09-22 16:37:43 +0100

    Add user list endpoint

---
+++ VerticalSliceArchitecture.API/Features/Users/GetUserList/GetUserListHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerticalSliceArchitecture.API.Abstractions;
+
+namespace VerticalSliceArchitecture.API.Features.Users.GetUserList;
+
+internal class GetUserListHandler(IHandlerContext ctx) : IRequestHandler
+{
+    public async Task<ErrorOr<GetUserListResponse>> HandleAsync(
+        GetUserListRequest request,
+        CancellationToken cancellationToken)
+    {
+        var total = await ctx.Database.Users.CountAsync(cancellationToken);
+        var users = await ctx.Database.Users
+            .AsNoTracking()
+            .OrderBy(user => user.UserId)
+            .Skip((request.Page - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetUserListResponse()
+        {
+            Users = users.Select(UserDto.Create),
+            Total = total,
+            Returned = users.Count,
+        };
+    }
+}
---
+++ VerticalSliceArchitecture.API/Features/Users/GetUserList/GetUserListModel.cs
@@ -0,0 +1,28 @@
+﻿using VerticalSliceArchitecture.Domain.Entities;
+
+namespace VerticalSliceArchitecture.API.Features.Users.GetUserList
+{
+    public record GetUserListRequest(int Page, int PageSize);
+
+    public record GetUserListResponse
+    {
+        public required IEnumerable<UserDto> Users { get; set; }
+        public required int Total { get; set; }
+        public required int Returned { get; set; }
+    }
+
+    public class UserDto
+    {
+        public required Guid UserId { get; set; }
+        public required string EmailAddress { get; set; }
+
+        public static UserDto Create(User user)
+        {
+            return new UserDto()
+            {
+                UserId = user.UserId,
+                EmailAddress = user.EmailAddress,
+            };
+        }
+    }
+}
