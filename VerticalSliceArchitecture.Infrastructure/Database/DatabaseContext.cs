410feff97c552322a073571a00428c0f44599614
Adam Nicol <adam.nicol@protonmail.com>
2026-09-25 10:35:46 +0100

    Filter deleted entities

--- VerticalSliceArchitecture.Infrastructure/Database/DatabaseContext.cs
+++ VerticalSliceArchitecture.Infrastructure/Database/DatabaseContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using VerticalSliceArchitecture.Domain.Abstractions;
 using VerticalSliceArchitecture.Domain.Entities;
+using VerticalSliceArchitecture.Domain.Enums;
 
 namespace VerticalSliceArchitecture.Infrastructure.Database
 {
@@ -10,6 +13,26 @@ namespace VerticalSliceArchitecture.Infrastructure.Database
         public DatabaseContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var type in builder.Model.GetEntityTypes()
+                .Where(x => typeof(ISoftDelete).IsAssignableFrom(x.ClrType)))
+            {
+                var method = typeof(DatabaseContext)
+                    .GetMethod(nameof(SoftDeleteFilter), BindingFlags.NonPublic | BindingFlags.Static)?
+                    .MakeGenericMethod(type.ClrType);
+
+                method?.Invoke(null, new[] { builder });
+            }
+        }
+
+        private static void SoftDeleteFilter<TEntity>(ModelBuilder builder) where TEntity : class, ISoftDelete
+        {
+            builder.Entity<TEntity>().HasQueryFilter(e => e.Status != EntityStatus.Deleted);
         }
     }
 }
